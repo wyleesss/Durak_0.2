dd3c88c6481ace46b10e47a2f2d76630a44325be
Koza_jp97@student.itstep.org <Koza_jp97@student.itstep.org>
2023-04-24 23:41:47 +0300

    Add project files.

---
+++ Durak_0.2/Durak.cs
@@ -0,0 +1,67 @@
+﻿namespace Durak
+{
+    class Player
+    {
+        public string name { get; }
+        public List<Card> koloda { get; set; } = new();
+        public List<SpecialAbilities> sa { get; }
+        public bool is_playing { get; set; }
+
+        public Player(string name, List<SpecialAbilities> sa, bool is_playing)
+        {
+            this.name = name;
+            this.sa = sa;
+            this.is_playing = is_playing;
+        }
+    }
+
+    class Game
+    {
+        Player player; // <-- зробив одного гравця. не бачу сенсу гратися з цілим списком. якщо не згідний - пиши
+
+        List<Card> koloda = new();
+        List<Card> rebound = new();
+
+        bool transferable;
+
+        public Game(List<Card> cards_buff, Player player, bool transferable)
+        {
+            this.player = player;
+            this.transferable = transferable;
+
+            Random random = new();
+            int random_index;
+
+            while (cards_buff.Count > 0)
+            {
+                random_index = random.Next(0, cards_buff.Count - 1);
+
+                koloda.Add(cards_buff[random_index]);
+                cards_buff.RemoveAt(random_index);
+            }
+        }
+
+        public void give_cards()
+        {
+            for (int i = 0; i < 6 - player.koloda.Count; i++)
+            {
+                player.koloda.Add(koloda[i]);
+                koloda.RemoveAt(i);
+            }
+        }
+
+        public void mixing()
+        {
+            Random random = new();
+            int random_index;
+
+            while (rebound.Count > 0)
+            {
+                random_index = random.Next(0, rebound.Count - 1);
+
+                koloda.Add(rebound[random_index]);
+                rebound.RemoveAt(random_index);
+            }
+        }
+    }
+}
---
+++ Durak_0.2/Program.cs
@@ -0,0 +1,96 @@
+﻿class Achivements
+{
+    public string name { get; }
+    public long award_money { get; }
+    public int golden_cards { get; }
+    public string type { get; }
+    public string game { get; }
+
+    public Achivements(string name, long award_money, int golden_cards,
+                       string type, string game)
+    {
+        this.name = name;
+        this.award_money = award_money;
+        this.golden_cards = golden_cards;
+        this.type = type;
+        this.game = game;
+    }
+}
+
+class SpecialAbilities
+{
+    string name { get; }
+    string type { get; }
+
+    public SpecialAbilities(string name, string type)
+    {
+        this.name = name;
+        this.type = type;
+    }
+    public void to_do() { /* нема */ }
+}
+
+class Shirt { } // <-- я пропоную це видалити нахуй (1)
+
+internal class User
+{
+    public int id { get; }
+    public string user_name { get; set; }
+    public string login { get; }
+    public string password { get; set; }
+    public string gmail { get; set; }
+
+    public long durak_b { get; set; }
+    public long black_jack_b { get; set; }
+
+    public int golden_cards { get; set; }
+
+    List<Shirt> shirts = new();
+    List<Achivements> achives = new();
+    List<SpecialAbilities> sa = new();
+
+    public User(int id, string user_name, string login, string password, string gmail,
+                long durak_b, long black_jack_b, int golden_cards,
+                List<Shirt> shirts, List<Achivements> achives, List<SpecialAbilities> sa)
+    {
+        this.id = id;
+        this.user_name = user_name;
+        this.login = login;
+        this.password = password;
+        this.gmail = gmail;
+        this.durak_b = durak_b;
+        this.black_jack_b = black_jack_b;
+        this.golden_cards = golden_cards;
+        this.shirts = shirts;
+        this.achives = achives;
+        this.sa = sa;
+    }
+}
+
+class Card
+{
+    string skin; // <-- я пропоную це видалити нахуй (2)
+    char suit { get; }
+    int number { get; }
+
+    public Card(int number, char suit)
+    {
+        this.number = number;
+        this.suit = suit;
+    }
+}
+
+internal struct StaticCards
+{
+    public static List<Card> koloda_24 = new() { /* нема */ };
+    public static List<Card> koloda_36 = new() { /* нема */ };
+    public static List<Card> koloda_54 = new() { /* нема */ };
+}
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("");
+    }
+}
